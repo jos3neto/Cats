5083cce4aeef6014dc87bbe50fdda37053792836
jos3barros <barros_j@yahoo.com>
2018-01-23 16:04:52 -0200

    Initial Commit

---
+++ Cats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cats
+//
+//  Created by Jose on 23/1/18.
+//  Copyright © 2018 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
